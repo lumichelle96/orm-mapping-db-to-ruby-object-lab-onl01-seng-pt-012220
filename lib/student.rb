263e5ef93bad4bddd49c3d3cc3f4fa8deef1f29d
Michelle Lu <lu.michelle96@icloud.com>
2020-04-27 21:44:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,14 +58,14 @@ class Student
       end
   end
 
-  def self.first_x_students_in_grade_10(num)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
       LIMIT ?
       SQL
 
-      DB[:conn].execute(sql, num).map do |row|
+      DB[:conn].execute(sql,x).map do |row|
         self.new_from_db(row)
       end
   end
